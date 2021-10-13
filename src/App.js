8c501c016e83cbfd537ccb013acafcf1542bcb88
alexandervsabel <avsabel@cnmc.org>
2021-10-13 16:10:44 -0400

    "modified heatmap to take in data from a file"

--- src/App.js
+++ src/App.js
@@ -1,86 +1,96 @@
 import Header from "./components/Header";
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ReactFileReader from 'react-file-reader';
-import ReactDOM from "react-dom";
-// import { Navbar, Col } from "materialize";
-import OneYearHeatMap from "./test_data/heatmaptest";
-import HeatMapDate from "react-d3-heatmap";
-import 'materialize-css/dist/css/materialize.min.css';
+import HeatMap from './components/chart/HeatMap.js';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
 
-import "./App.css";
+const Input = styled('input')({
+    display: 'none',
+});
 
-class data{
-    constructor(date, number){
-        this.date = date
-        this.count = number
-    }
-}
 
 function App() {
     const [selectedFile, setSelectedFile] = useState();
     const [isFilePicked, setIsFilePicked] = useState(false);
-
+    const [data, setData] = useState([]);
     const changeHandler = (event) => {
         setSelectedFile(event.target.files[0]);
         setIsFilePicked(true);
     };
 
     const handleSubmission = () => {
-        console.log(selectedFile)
         const reader = new FileReader()
         reader.onload = async (e) => {//find and accept file
             const text = (e.target.result)
             alert('File Accepted')
         }
         reader.readAsText(selectedFile)
-        reader.onload = function(e) {
+        reader.onload = function (e) {
             var fileContentArray = this.result.split(/\n/)//split every line
             fileContentArray.pop()
             var finalArray = []
-            for(var i=0; i<fileContentArray.length;i++){//for every timepoint
+            for (var i = 0; i < fileContentArray.length; i++) {//for every timepoint
                 var temp = []
                 var splitstring = fileContentArray[i].split('  ')//split ROIs
-                for(var x=0; x<200;x++){//arrange each ROI in an array as a float
+                for (var x = 0; x < 200; x++) {//arrange each ROI in an array as a float
+                    finalArray.push({ "group": "r" + i, variable: "c" + x, "value": splitstring[x] })
                     temp.push(parseFloat(splitstring[x]))
                 }
                 finalArray.push(temp)//array of each timepoint; each cell is an array of ROIs(floats)
             }
             var stringArray = reader.result.split('  ')
             var floatArray = []
-            console.log(finalArray)
-            for(var i =0; i<stringArray.length;i++){
+            for (var i = 0; i < stringArray.length; i++) {
                 floatArray.push(parseFloat(stringArray[i]));
             }
-
+            setData(finalArray)
+            console.log(data)
         }
     };
 
 
-  return (
-      <div>
-          <input type="file" name="file" accept=".1D, .txt" onChange={changeHandler} />
-          {isFilePicked ? (
-              <div>
-                  nameOfFile = selectedFile.name
-                  <p>Filename: {selectedFile.name}</p>
-                  <p>Filetype: {selectedFile.name.split('.').pop()}</p>
-                </div>
-          ) : (
-              <p>Select a file to show details</p>
-          )
-          }
-          <div>
-              <button onClick={handleSubmission}>Submit</button>
-          </div>
-          <Header title='Upload the time series data. The data must be in a .txt or .1D format.'/>
-          
-    <script src="https://d3js.org/d3.v4.js"></script>
+    return (
+        <StyledEngineProvider injectFirst>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                        <label htmlFor="contained-button-file">
+                            {/* <Input type="file" name="file" accept=".1D, .txt" onChange={changeHandler} /> */}
+                            <Input accept=".1D" id="contained-button-file" multiple type="file" onChange={changeHandler} />
+                            <Button variant="contained" component="span">
+                                Upload
+                            </Button>
+                        </label>
+                        {isFilePicked ? (
+                            <div>
+                                nameOfFile = selectedFile.name
+                                <p>Filename: {selectedFile.name}</p>
+                                <p>Filetype: {selectedFile.name.split('.').pop()}</p>
+                            </div>
+                        ) : (
+                            <p>Select a file to show details</p>
+                        )
+                        }
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Button onClick={handleSubmission}>Submit</Button>
+                    </Grid>
+                    <HeatMap data={data} />
+                    
+                    <Grid item xs={4}>
+                        { 
+                        <HeatMap data={data} /> 
+                        }
+                    </Grid>
+                </Grid>
+            </Box>
 
-    <div id="my_dataviz"></div>
-    var input = document.getElementById("heatmap");
-    input.value = 1;
-    </div>
-  )
+        </StyledEngineProvider>
+    )
 }
 
 export default App;
---
+++ src/components/chart/HeatMap.js
@@ -0,0 +1,85 @@
+import React from "react";
+import * as d3 from 'd3';
+import Grid from '@mui/material/Grid';
+
+class HeatMap extends React.Component {
+    constructor(props) {
+        super(props);
+        this.myRef = React.createRef()
+    }
+
+    componentDidMount() {
+        console.log(this.props.data)
+    }
+
+    componentDidUpdate() {
+        console.log(this.props.data)
+        // set the dimensions and margins of the graph
+        const margin = { top: 0, right: 0, bottom: 0, left: 0 },
+            width = 700 - margin.left - margin.right,
+            height = 450 - margin.top - margin.bottom;
+
+        // append the svg object to the body of the page
+        // append the svg object to the body of the page
+        const svg = d3.select(this.myRef.current)
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+        let data = this.props.data
+        //Read the data
+
+        // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
+        const myGroups = Array.from(new Set(data.map(d => d.group)))
+        const myVars = Array.from(new Set(data.map(d => d.variable)))
+
+        // Build X scales and axis:
+        var x = d3.scaleBand()
+            .range([0, width])
+            .domain(myGroups)
+            .padding(0);
+        svg.append("g")
+            .style("font-size", 15)
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x).tickSize(0))
+            .select(".domain").remove()
+
+        // Build X scales and axis:
+        var y = d3.scaleBand()
+            .range([height, 0])
+            .domain(myVars)
+            .padding(0);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        // Build color scale
+        var myColor = d3.scaleLinear()
+            .range(["white", "#A52A2A"])
+            //.range(["blue","green"])
+            .domain([-20, 20])
+
+        svg.selectAll()
+            .data(data, function (d) { return d.group + ':' + d.variable; })
+            .enter()
+            .append("rect")
+            .attr("x", function (d) { return x(d.group) })
+            .attr("y", function (d) { return y(d.variable) })
+            .attr("width", x.bandwidth())
+            .attr("height", x.bandwidth())
+            .style("fill", function (d) { return myColor(d.value) })
+
+    }
+
+    render() {
+        return (
+            <>
+                <Grid item xs={8} ref={this.myRef}>
+                    </Grid>
+            </>
+        )
+    }
+}
+
+export default HeatMap
